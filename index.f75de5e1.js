70124bcc1129b796e6896304302df7527d1d8836
Yuliia <juliapushkin6@gmail.com>
2025-01-11 22:11:27 +0200

    make build

--- index.f75de5e1.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("hashchange", ()=>{
-    if (window.location.hash === "#menu") document.body.classList.add("page__body--with-menu");
-    else document.body.classList.remove("page__body--with-menu");
-});
-
-//# sourceMappingURL=index.f75de5e1.js.map
